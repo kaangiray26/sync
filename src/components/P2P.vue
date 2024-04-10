4b088375b8991df19d1cce9eefe9fadbedd3c488
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2024-04-10 07:40:43 +0200

    zero pad

--- src/components/P2P.vue
+++ src/components/P2P.vue
@@ -51,14 +51,16 @@ const progress = ref(0);
 const remainingTime = ref(0);
 
 function formatDuration(time) {
+    console.log(time);
     let hours = Math.floor(time / 3600);
     let minutes = Math.floor((time - (hours * 3600)) / 60);
     let seconds = Math.floor(time - (hours * 3600) - (minutes * 60));
 
-    if (hours < 10) { hours = "0" + hours; }
-    if (minutes < 10) { minutes = "0" + minutes; }
-    if (seconds < 10) { seconds = "0" + seconds; }
-    if (hours) { hours = "00"; }
+    // Zero pad
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
     return hours + ':' + minutes + ':' + seconds;
 }
 
